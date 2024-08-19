6e4a20f8e814b35f46ae089a5acba64f2aea8654
Sadman Sakib <sakibsadman108@gmail.com>
2024-08-19 11:40:08 +0600

    Update SinglePlan.tsx

--- src/components/ui/SinglePlan.tsx
+++ src/components/ui/SinglePlan.tsx
@@ -1,64 +1,61 @@
-"use client";
+"use client"
 
-// src/components/MonthlySubscription.tsx
-import { cancelSubscription, getSubscription } from "@/api/agent_1";
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { cancelSubscription, getSubscription } from "../../api/agent_1";
 import {
 	Button,
 	Card,
 	CardBody,
 	CardFooter,
-	Divider,
 	Heading,
-	ModalOverlay,
 	Stack,
 	Text,
 	useDisclosure,
 	useToast,
-} from "@/components/bootstrap/index";
-import { setSubscription } from "@/redux/features/auth/auth.slice";
-import { useAppDispatch } from "@/redux/hooks";
-import { RootState } from "@/redux/store";
-import socket from "@/socket";
-import { IPlan } from "@/types/plans.types";
-import { IUser } from "@/types/user.types";
+} from "../bootstrap/index";
+
 import { List, ListIcon, ListItem } from "@chakra-ui/react";
-import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 import { AxiosError } from "axios";
 import { useEffect, useState } from "react";
 import { IoMdCheckmarkCircle } from "react-icons/io";
 import { useMutation, useQuery } from "react-query";
 import { connect } from "react-redux";
+import { setSubscription } from "../../redux/features/auth/auth.slice";
+import { useAppDispatch } from "../../redux/hooks";
+import { RootState } from "../../redux/store";
+import socket from "../../socket";
+import { IPlan } from "../../types/plans.types";
+import { IUser } from "../../types/user.types";
+import stripePromise from "../../utils/stripePromise";
 import CheckoutModal from "./CheckoutModal";
 import CoreAlertDialog from "./CoreAlertDialog";
-
-const stripePromise = loadStripe("YOUR_STRIPE_PUBLIC_KEY");
 
 interface IData {
 	priceId: string;
 	subId: string;
 }
 
-const OverlayOne = () => (
-	<ModalOverlay
-		bg="blackAlpha.300"
-		backdropFilter="blur(10px) hue-rotate(90deg)"
-	/>
-);
-
 const SinglePlan: React.FC<{ plan: IPlan; currentUser: IUser | null }> = ({
 	plan,
 	currentUser,
 }) => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
-	const [overlay, setOverlay] = useState(<OverlayOne />);
 	const [subPriceId, setSubPriceId] = useState<IData | null>(null);
 
-	const { data, isLoading, refetch, isError } = useQuery<any, AxiosError>(
+	interface IResponse {
+		data: {
+			id: string;
+			plan: {
+				id: string;
+			};
+		};
+	}
+
+	const { data, refetch, isError } = useQuery<IResponse, AxiosError>(
 		"subscription",
-		getSubscription,
-		{
-			retryOnMount: false,
-		}
+		getSubscription
 	);
 
 	useEffect(() => {
@@ -74,42 +71,46 @@ const SinglePlan: React.FC<{ plan: IPlan; currentUser: IUser | null }> = ({
 	}, [data, currentUser, isError]);
 
 	useEffect(() => {
+
 		if (!currentUser) return;
 
 		const email = currentUser.email;
 
+		const handler = () => refetch();
+		const setter = () => setSubPriceId(null);
 
-		socket.on(`user:subscription:${email}`, (data) => {
+		socket.on(`user:subscription:${email}`, () => {
 			refetch();
 		});
 
-		socket.on(`user:unsubscribe:${email}`, (data) => {
+		socket.on(`user:unsubscribe:${email}`, () => {
 			setSubPriceId(null);
 		});
 
 		return () => {
-			socket.off(`user:subscription:${email}`);
-			socket.off(`user:unsubscribe:${email}`);
+			socket.off(`user:subscription:${email}`, handler);
+			socket.off(`user:unsubscribe:${email}`, setter);
 		};
 	}, [currentUser, data]);
 
 	const handleSubscribe = () => {
-		setOverlay(<OverlayOne />);
 		onOpen();
 	};
 	const toast = useToast();
 	const dispatch = useAppDispatch();
 
-	const { mutate, isLoading: isCancelLoading } = useMutation<IUser, any>(
+	const { mutate, isLoading: isCancelLoading } = useMutation(
 		"cancelSubscription",
 		{
-			mutationFn: () => cancelSubscription(subPriceId?.subId as string),
+			mutationFn: () =>
+				cancelSubscription(currentUser?.subscriptionId as string),
 			onSuccess: (data) => {
 				setSubPriceId(null);
 				socket.emit("unsubscribe", { ...data });
 				refetch();
 			},
-			onError: (error) => {
+
+			onError: (error: any) => {
 				toast({
 					title: "Error",
 					description: error.response.data.message,
@@ -128,23 +129,26 @@ const SinglePlan: React.FC<{ plan: IPlan; currentUser: IUser | null }> = ({
 
 	return (
 		<>
-			<CheckoutModal
-				refetchSubscription={refetch}
-				overlay={overlay}
-				isOpen={isOpen}
-				onClose={onClose}
-				plan={plan}
-			/>
+			<Elements stripe={stripePromise}>
+				<CheckoutModal
+					refetchSubscription={refetch}
+					isOpen={isOpen}
+					onClose={onClose}
+					plan={plan}
+				/>
+			</Elements>
 			<Card
-				className={`w-full ${
-					plan.name == "Monthly" ? "bg-primary" : "bg-warning"
-				} ${plan.name == "Monthly" ? "text-white" : "text-dark"}`}
+				className="w-auto px-12 bg-primary text-white"
+				bgColor={"bg-primary"}
+				textColor={"text-white"}
 			>
 				<CardBody>
 					<Stack mt="6" spacing="3">
-						<Heading size="lg">{plan.name}</Heading>
-						<Text>{plan.description}</Text>
-						<List spacing={3}>
+						<div className="text-center">
+							<Heading size="xl">{plan.name}</Heading>
+							<Text>{plan.description}</Text>
+						</div>
+						<List spacing={3} className="flex flex-col gap-4 text-xl py-4">
 							{plan.features.map((item, i) => (
 								<ListItem key={i}>
 									<ListIcon as={IoMdCheckmarkCircle} color="green.500" />
@@ -153,37 +157,29 @@ const SinglePlan: React.FC<{ plan: IPlan; currentUser: IUser | null }> = ({
 							))}
 						</List>
 						<Text
+							className="text-center"
 							color={plan.name == "Monthly" ? "white" : "black"}
-							fontSize="4xl"
+							fontSize="6xl"
 						>
 							{plan.price}
 						</Text>
 					</Stack>
 				</CardBody>
-				<Divider />
 				<CardFooter>
-					{plan.priceId == subPriceId?.priceId ? (
-						// <Button
-						// 	className={`p-2 font-semibold rounded-md bg-secondary hover:text-slate text-white`}
-						// 	variant="solid"
-						// 	onClick={handleCancelSubscription}
-						// 	isLoading={isCancelLoading}
-						// >
-						// 	Cancel subscription
-						// </Button>
+					{currentUser?.subscriptionId == subPriceId?.subId ? (
 						<CoreAlertDialog
 							isLoading={isCancelLoading}
 							alertHeader="Cancel Subscription"
 							disclosureFn={useDisclosure}
 							handlerFn={handleCancelSubscription}
 							textTrgigger="Cancel Subscription"
-							classNames="p-2 font-semibold rounded-md bg-secondary hover:text-slate text-white"
+							classNames="mx-auto p-2 font-semibold rounded-md bg-secondary hover:text-slate text-white"
 						>
 							<span>Do you want to unscribe?</span>
 						</CoreAlertDialog>
 					) : (
 						<Button
-							className={`p-2 font-semibold rounded-md bg-danger hover:text-slate text-white`}
+							className={`mx-auto p-2 font-semibold rounded-md bg-danger hover:text-slate text-white`}
 							variant="solid"
 							onClick={handleSubscribe}
 						>
