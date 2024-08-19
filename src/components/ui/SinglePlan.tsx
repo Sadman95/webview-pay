6aa309a5ed6373f85c49d24d9abcd90df8f51ac5
Sadman Sakib <sakibsadman108@gmail.com>
2024-08-19 11:43:30 +0600

    Update SinglePlan.tsx

--- src/components/ui/SinglePlan.tsx
+++ src/components/ui/SinglePlan.tsx
@@ -1,7 +1,5 @@
 "use client"
 
-/* eslint-disable react-refresh/only-export-components */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { cancelSubscription, getSubscription } from "../../api/agent_1";
 import {
 	Button,
